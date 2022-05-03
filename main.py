82eee95ec3148e5e834ebae0cb43b2ee70f0de64
alexw <alexwendland@me.com>
2022-05-03 09:05:42 -0500

    initial commit

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 from flask import Flask, abort, request, Response
-import redis
 import requests
 import json
 import base64
 import hashlib
 import hmac
 import os
-import rq
-import re
 import time
 from dotenv import load_dotenv
 
@@ -16,11 +13,8 @@ from hmacSHA1 import generate_hash_bytes
 load_dotenv()
 test_webhook    = os.getenv('NGROK', None)
 signing_key = os.getenv('WEBHOOK_SECRET_KEY', None)
-redis_conn = redis.from_url(os.getenv('REDIS_URL', 'redis://localhost:6379'))
-in_queue = rq.Queue('handle_sms', connection=redis_conn)
-out_queues = [
-    rq.Queue('send_confirmation', connection=redis_conn)
-    ]
+
+app = Flask(__name__)
 
 # Webhook documentation
 # https://developers.eztexting.com/docs/webhooks-1
@@ -30,6 +24,7 @@ out_queues = [
     This header can then be used to verify the callback POST \
     is coming from EZ Texting.
 """
+
 
 def validate_hmac_header(header, body, signing_key: str):
     hashed_body_bytes = generate_hash_bytes(body, signing_key) # returns a hashlib object
@@ -48,17 +43,6 @@ def validate_hmac_header(header, body, signing_key: str):
 
     return False
 
-def send_confirmation(fromNumber, group):
-    print(f'Congratulations {fromNumber}! You have signed up for {group}')
-
-def extract_weekday_from_sms(message: str, fromNumber: str=None, id: str=None):
-    days_of_week_regex_pattern = re.compile('(mon|tues|wednes|thurs|fri|satur|sun)day')
-    signup_day_matches = re.findall(days_of_week_regex_pattern, message)
-    
-    for availability_bucket in signup_day_matches:
-        out_queues[0].enqueue(send_confirmation, fromNumber, availability_bucket)
-
-app = Flask(__name__)
 
 @app.route('/inbound_sms_received', methods=['POST'])
 def handle_sms():
